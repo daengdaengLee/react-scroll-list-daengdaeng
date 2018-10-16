942e936dffaba5b2b39a118efd8a6bfca5d9dbf0
daengdaengLee <gunho1020@gmail.com>
2018-10-17 08:35:00 +0900

    ENH: Add scroll event

--- src/components/2-molecules/scroll-list/index.js
+++ src/components/2-molecules/scroll-list/index.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class ScrollList extends Component {
+  constructor(props) {
+    super(props);
+    this._onScroll = this._onScroll.bind(this);
+  }
+
   render() {
+    const { _onScroll } = this;
     const { list, containerStyle, perPage, fromIdx, renderItem } = this.props;
     const currentPage = list.slice(fromIdx, fromIdx + perPage);
     return (
-      <div style={{ ...containerStyle, overflow: 'auto' }}>
+      <div style={{ ...containerStyle, overflow: 'auto' }} onScroll={_onScroll}>
         {currentPage.map(renderItem)}
       </div>
     );
+  }
+
+  _onScroll(event) {
+    const {
+      target: { scrollTop, scrollHeight, clientHeight },
+    } = event;
+    const { onScroll, onScrollToTop, onScrollToBottom } = this.props;
+    onScroll(event);
+    if (scrollTop <= 0) {
+      onScrollToTop();
+    } else if (scrollTop + clientHeight >= scrollHeight) {
+      onScrollToBottom();
+    }
   }
 }
 
@@ -18,6 +37,9 @@ ScrollList.defaultProps = {
   containerStyle: {},
   perPage: 10,
   fromIdx: 0,
+  onScroll: event => {},
+  onScrollToTop: () => {},
+  onScrollToBottom: () => {},
   renderItem: (item, idx, currentPage) => {},
 };
 
@@ -26,6 +48,9 @@ ScrollList.propTypes = {
   containerStyle: PropTypes.object,
   perPage: PropTypes.number,
   fromIdx: PropTypes.number,
+  onScroll: PropTypes.func,
+  onScrollToTop: PropTypes.func,
+  onScrollToBottom: PropTypes.func,
   renderItem: PropTypes.func,
 };
 
