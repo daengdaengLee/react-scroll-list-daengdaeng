1eb16f1f7ec7e3facfa27d0e6f48cf36a3071452
daengdaengLee <gunho1020@gmail.com>
2018-10-16 16:43:05 +0900

    ENH: Update scroll-list comp

--- src/components/2-molecules/scroll-list/index.js
+++ src/components/2-molecules/scroll-list/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ScrollList = () => <div>Hello React</div>;
+const ScrollList = ({ list, containerStyle, perPage, pageIdx, renderItem }) => {
+  const currentPage = list.slice(pageIdx * perPage, (pageIdx + 1) * perPage);
+  return (
+    <div style={{ ...containerStyle, overflow: 'auto' }}>
+      {currentPage.map(renderItem)}
+    </div>
+  );
+};
+
+ScrollList.defaultProps = {
+  list: [],
+  containerStyle: {},
+  perPage: 10,
+  pageIdx: 0,
+  renderItem: (item, idx, currentPage) => {},
+};
+
+ScrollList.propTypes = {
+  list: PropTypes.array,
+  containerStyle: PropTypes.object,
+  perPage: PropTypes.number,
+  pageIdx: PropTypes.number,
+  renderItem: PropTypes.func,
+};
 
 export default ScrollList;
