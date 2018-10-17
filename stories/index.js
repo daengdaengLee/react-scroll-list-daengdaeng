bc58ee484a7fa72e62c36360250cd7c7905b59f0
daengdaengLee <gunho1020@gmail.com>
2018-10-17 12:35:29 +0900

    TST: Register controll-from-idx-and-per-page story

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import ControllFromIdxAndPerPage from './controll-from-idx-and-per-page';
 import Default from './default';
 
-storiesOf('Basic', module).add('default', () => <Default />);
+storiesOf('Basic', module)
+  .add('default', () => <Default />)
+  .add('controll fromIdx and perPage', () => <ControllFromIdxAndPerPage />);
