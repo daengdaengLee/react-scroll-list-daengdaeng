27d76819bcd3a1d542007c1a87e8f8c5e8a11736
daengdaengLee <gunho1020@gmail.com>
2018-10-17 16:09:20 +0900

    TST: Make scroll-like-excel story

--- stories/index.js
+++ stories/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ControllFromIdxAndPerPage from './controll-from-idx-and-per-page';
 import Default from './default';
+import ScrollLikeExcel from './scroll-like-excel';
 
 storiesOf('Basic', module)
   .add('default', () => <Default />)
-  .add('controll fromIdx and perPage', () => <ControllFromIdxAndPerPage />);
+  .add('controll fromIdx and perPage', () => <ControllFromIdxAndPerPage />)
+  .add('scroll like excel', () => <ScrollLikeExcel />);
